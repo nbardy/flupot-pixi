3966b7c9d631e3ff049502092173fedd2cd2dd37
Nicholas Bardy <nicholasbardy@yahoo.com>
2016-09-14 17:04:32 -0400

    Update react-pixi dependency
    *Non-breaking

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.228"]
                  [flupot "0.4.0"]
-                 [cljsjs/react-pixi "0.8.1-0"]]
+                 [cljsjs/react-pixi "0.8.2-0"]]
   :plugins [[lein-cljsbuild "1.1.3"]]
   :cljsbuild
   {:builds {:main {:source-paths ["src"]
